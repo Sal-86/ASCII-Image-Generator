589854dff0a4ef5f346162c36444fbdce4b7971b
LILY SCHEERER <scheererlily@gmail.com>
2025-04-30 21:45:31 -0400

    Populate array with RGB data.

--- src/ASCII_Generator.java
+++ src/ASCII_Generator.java
@@ -39,14 +39,33 @@ public final class ASCII_Generator {
     }
 
     /**
-     * Determines and prints pixel dimensions of image
-     * @param image
-     *             Buffered image that has been processed
+     * 
+     * @param w
+     *          the image's width in pixels
+     * @param h
+     *          the image's height in pixels
      */
-    private static void imageDimensions(BufferedImage image){
-        int width = image.getWidth();
-        int height = image.getHeight();
-        System.out.println("Image pixel dimensions= "+ width + ", "+height);
+    private static void populateArray(int w, int h, BufferedImage image) {
+        int[][][] rgbArray = new int[h][w][3];
+
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+
+                // RGB value for each pixel
+                int pixel = image.getRGB(x,y);
+
+                // Separate binary (AAAAAAAARRRRRRRRGGGGGGGGBBBBBBBB) into correct components
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+
+                // Store values in array
+                rgbArray[y][x][0] = red;
+                rgbArray[y][x][1] = green;
+                rgbArray[y][x][2] = blue;
+            }
+        }
+        System.out.print("Successful iteration, RGB data stored!");
     }
 
     /**
@@ -69,11 +88,12 @@ public final class ASCII_Generator {
             }
 
         // Get image dimensions
-        imageDimensions(image);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        System.out.println("Dimensions are: " + width + " x " + height);
 
         // Populate 2D matrix with numerical brightness data for each pixel
-        
-        
+        populateArray(width, height, image);
 
     }
 }
